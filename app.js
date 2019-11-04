3c76e6be6515a58f5c3af3978dc979c3f63f0bc4
victorocna <victorocnarescu@gmail.com>
2019-11-04 17:36:40 +0200

    try to export express app on a serverless architecture

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ const app = express();
 const port = 5678;
 
 app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/hello", (req, res) => res.send("Hello World Again!"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+module.exports = app
