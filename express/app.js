1c692b014c814d0c0aa63dee032e3f044b5d90b7
victorocna <victorocnarescu@gmail.com>
2019-11-04 17:53:18 +0200

    add example from original docs

--- express/app.js
+++ express/app.js
@@ -1,10 +1,22 @@
-const express = require("express");
+'use strict';
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
 const app = express();
-const port = 5678;
+const bodyParser = require('body-parser');
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.get("/hello", (req, res) => res.send("Hello World Again!"));
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
-module.exports = app
+module.exports = app;
+module.exports.handler = serverless(app);
